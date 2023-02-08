822ffcaa454bb0a115fda7f78727350d17b8dabe
yinzhijie <yzjgogo@139.com>
2023-02-08 16:57:07 +0800

    提一次提交

---
+++ pages/1_databind1/1_databind1.js
@@ -0,0 +1,69 @@
+// pages/1_databind1/1_databind1.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info:'初始化数据',
+    msgList:[{msg:"成都"},{msg:"你好"}],
+    imgSrc:"https://img0.baidu.com/it/u=4049075139,1006912346&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200",
+    randomNum1:Math.random()*10
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
